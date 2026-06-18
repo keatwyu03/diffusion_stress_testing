01ee276f28ab779226b1b33e8edc8f510c316521
Kwun Yu (Kaden) Wu <kw2963@nyu.edu>
2026-06-17 22:55:17 -0700

    Updates

--- explore/import_data.py
+++ explore/import_data.py
@@ -19,7 +19,7 @@ sp500 = yf.download('^GSPC', start = '2008-01-01')['Close'].squeeze()
 
 
 data = {
-    'aaa': fred.get_series('AAA'),
+    'gs5': fred.get_series('GS5'),
     'gdp': fred.get_series('GDP'),
     'spread': fred.get_series('T10Y2Y'),
     'sp500': fred.get_series('SP500'),
